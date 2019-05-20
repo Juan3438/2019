2ab57a144e7110380a77046819b73407b83289b1
BT_1N3_24 <ljaoutakas8934@bths.edu>
2019-05-20 14:37:13 -0400

    asdfasd

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -1,20 +1,44 @@
 public class APCalendar
 {
 
-    private static int dayOfYear(int month, int day, int year)
+    private static int dayOfYear(int inMonth, int inDay, int inYear)
     {
-        return r(1+5* r(year - 1,4 ) + 4 * r(year-1,100) + 6*r(year-1,400),7);
+
+        int  month, year, day,c;
+        year = inYear;
+        day = inDay;
+
+        if (inMonth < 3) {
+            year--;
+            inMonth = inMonth +10;
+        }
+        else {
+            inMonth = inMonth -2;
+        }
+
+        month = inMonth;
+
+
+        c = year/100;
+        year = year%100;
+
+        int A = (int) (day+((2.6*month)-0.2)+year+(year/4)+(c/4)-(2*c));
+
+        int x = A % 7;
+
+        if (x < 0) {
+            x+=7;
+        }
+
+        return x;
     }
 
 
 
     public static void main(String [] args)
     {
-        System.out.println(dayOfYear(5,1,2019));
+        System.out.println(dayOfYear(5,18,2019));
     }
-    private static int r(int x,int y)
-    {
-        return x%y;
-    }
+
 
 }
