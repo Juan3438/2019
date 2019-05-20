08695d0491b28feb3a044abdd0fbc2487761f80e
BT_1N3_24 <ljaoutakas8934@bths.edu>
2019-05-20 14:33:36 -0400

    asdfasd

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -1,5 +1,20 @@
 public class APCalendar
 {
-    
+
+    private static int dayOfYear(int month, int day, int year)
+    {
+        return r(1+5* r(year - 1,4 ) + 4 * r(year-1,100) + 6*r(year-1,400),7);
+    }
+
+
+
+    public static void main(String [] args)
+    {
+        System.out.println(dayOfYear(5,1,2019));
+    }
+    private static int r(int x,int y)
+    {
+        return x%y;
+    }
 
 }
